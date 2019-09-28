8860a5093d1251f70e908906c448526880e420f5
Kelly Meier <kemeier2000@yahoo.com>
2019-09-28 12:49:06 -0500

    added code for connecting selection to database

--- bamazon.js
+++ bamazon.js
@@ -2,45 +2,46 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "9Tachyons",
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-    console.log("Connected as id: "+connection.threadId);
+connection.connect(function (err) {
+    console.log("Connected as id: " + connection.threadId);
     queryallproducts();
 });
 
 function queryallproducts() {
-    connection.query("SELECT * FROM products",function(err,res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
         }
-            console.log("-----------------------------------");
-            inquirer.prompt([{
-                name:"itemID",
-                type:"input",
-                message:"What is ID of the item you'd like to buy?"
-            },{ 
-                name:"unitAmount",
-                type:"input",
-                message:"How many units of the item would you like?"
-            }]);
-    })
-        .then(function(answer) {
-            var query = "SELECT * FROM products where ID=?" 
-            connection.query(query, { product: answer.itemID }, function(err, res) {
-                if (err) throw err;
-                if (res.length === 0) {
-                    console.log("Invalid product number")
-                };
+        console.log("-----------------------------------");
+        inquirer.prompt([{
+            name: "itemID",
+            type: "input",
+            message: "What is ID of the item you'd like to buy?"
+        }, {
+            name: "unitAmount",
+            type: "input",
+            message: "How many units of the item would you like?"
+        }])
+            .then(function (answer) {
+                var query = "SELECT * FROM products where ?"
+                connection.query(query, { item_id: answer.itemID }, function (err, res) {
+                    if (err) throw err;
+                    if (res.length === 0) {
+                        console.log("Invalid product number")
+                    };
+                    console.log("Yay!");
+                    connection.end();
+                });
             });
-        });
-
+    });
 }
 
 
@@ -50,4 +51,3 @@ function queryallproducts() {
 
 
 
-    
